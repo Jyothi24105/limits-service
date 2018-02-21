930cb2d3af4bc718ffb0de5e85abaaf297191c9c
jyothi <jyothi@jo-macbook-pro>
2018-02-20 23:56:41 -0500

    first

---
+++ src/main/java/com/example/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -0,0 +1,22 @@
+
+package com.example.microservices.zipkindistributedtracingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+
+/**
+ * @author jyothi
+ *
+ */
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinDistributedTracingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
+	}
+}
